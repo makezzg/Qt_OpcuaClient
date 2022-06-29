00f5f774fad0c4d9868420918197dcfecb6b1240
Zhu Zuguang <makezzg@qq.com>
2022-06-29 11:15:38 +0800

    删除不必要的信号和槽，以及相对应的控件

--- mainwindow.h
+++ mainwindow.h
@@ -22,23 +22,17 @@ public:
     void chgEnable(boolean enable);         //function that set/reset the widgets' enable attribute on window
 
 private slots:
-    void on_pushButton_connect_clicked();
+//    void on_pushButton_connect_clicked();
 
-    void on_pushButton_connect_2_clicked();
+//    void on_pushButton_string_write_clicked();
 
-    void on_pushButton_string_read_clicked();
+//    void on_pushButton_string_read_2_clicked();
 
-    void on_pushButton_string_write_clicked();
+//    void on_pushButton_add_value_clicked();
 
-    void on_pushButton_string_read_2_clicked();
+//    void on_pushButton_string_read_4_clicked();
 
-    void on_pushButton_add_value_clicked();
-
-    void on_pushButton_string_read_4_clicked();
-
-    void on_pushButton_string_read_5_clicked();
-
-    void on_pushButton_string_read_6_clicked();
+//    void on_pushButton_string_read_5_clicked();
 
     void on_BtnWriteInt_clicked();
 
